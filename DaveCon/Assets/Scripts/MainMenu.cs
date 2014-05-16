555a2c24fe2cbc5ef006ad15b66ee296ba2bb4f6
LevityDave <Seven_Years_Later@hotmail.com>
2014-05-16 21:55:18 +0100

    next File
    Sorry

---
+++ DaveCon/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(10,10,200,30),"Quit"))
+		{
+			Application.Quit();
+		}
+	}
+}
