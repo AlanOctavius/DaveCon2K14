69fa8643714a518e5a6d635cb8fa4a510abab137
LevityDave <Seven_Years_Later@hotmail.com>
2014-05-17 22:35:13 +0100

    Game Lobby Created
    Server Browser and game lobby created allowing people to join game in
    new scene.

---
+++ DaveCon/Assets/Scripts/ServerBrowser.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerBrowser : MonoBehaviour {
+
+	private float w;
+	private float h;
+	
+	private Rect rect;
+
+	private bool isRefreshing = false;
+	private float refreshRequestLength = 3.0f;
+	HostData[] hostData;
+
+	private string registeredGameName = "SC_DaveCon_Network_Test_Server";
+	private Vector2 scrollPosition;
+	// Use this for initialization
+	void Start () {
+		w = PlayerPrefs.GetFloat ("defaultWidth");
+		h = PlayerPrefs.GetFloat ("defaultHeight");
+		registeredGameName = PlayerPrefs.GetString("registeredGameName");
+
+	}
+
+	public IEnumerator RefreshHostList()
+	{
+		Debug.Log ("Refreshing ... ");
+		MasterServer.RequestHostList (registeredGameName);
+		
+		float timeStarted = Time.time;
+		float timeEnd = Time.time + refreshRequestLength;
+		
+		while (Time.time < timeEnd)
+		{
+			hostData = MasterServer.PollHostList ();
+			yield return new WaitForEndOfFrame ();
+		}
+		
+		if(hostData == null || hostData.Length == 0)
+		{
+			Debug.Log("No Active Servers have been found");
+		}
+		else
+		{
+			Debug.Log(hostData.Length + " Servers have been found");
+		}
+	}
+	
+	// Update is called once per frame
+	public void OnGUI()
+	{
+
+		GUIStyle style = new GUIStyle ();
+		style.alignment=TextAnchor.MiddleCenter;
+		rect.x = (Screen.width*(1-w))/2;
+		rect.y = (Screen.height*(1-h))/2;
+		rect.width = Screen.width*w;
+		rect.height = Screen.height*h;
+
+		GUILayout.BeginArea(rect);
+		{
+			GUILayout.BeginVertical(); // also can put width in here
+			{
+				if (GUILayout.Button("Refresh Server List")) // also can put width here
+				{
+					Debug.Log("Refresh Server List Button Clciked");
+					StartCoroutine("RefreshHostList");
+				}
+				if (GUILayout.Button("Back")) // also can put width here
+				{
+					Debug.Log("Back button pressed from Server Browser, Loading Multiplayer Menu.");
+					Application.LoadLevel("MuliplayerMenu");
+				}
+				
+			}
+			GUILayout.EndVertical();
+		}
+		GUILayout.EndArea();
+
+		rect.y = (3*Screen.height*(1-h))/4;
+		GUILayout.BeginArea (rect);
+		{
+			GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(rect.width), GUILayout.Height(rect.height));
+			{
+				if(hostData != null)
+				{
+					GUILayout.BeginVertical();
+					{
+						for (int i = 0; i < hostData.Length; i++)
+						{
+							GUILayout.BeginHorizontal(); // also can put width in here
+							{
+								GUILayout.Label(hostData[i].gameName);
+								GUILayout.Label("players " + hostData[i].connectedPlayers);
+								if(GUILayout.Button("Join"))
+								{
+									Network.Connect(hostData[i]);
+									Application.LoadLevel("GameLobby");
+								}
+							}
+							GUILayout.EndHorizontal();
+						}
+					}
+					GUILayout.EndVertical();
+				}
+			}
+			GUILayout.EndScrollView ();
+		}
+		GUILayout.EndArea ();
+	}
+}
---
+++ DaveCon/Assets/Scripts/SetDefaults.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetDefaults : MonoBehaviour {
+
+	public float width = 0.3f;
+	public float height = 0.3f;
+
+	public string ServerName = "DaveCon Server";
+	public string connectionPort = "25001";
+
+	public string playersHardcap = "8";
+	public string maxPlayers = "4";
+
+	public string registeredGameName = "SC_DaveCon_Network_Test_Server";
+
+	public string connectionIP;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		// Screen
+		Debug.Log ("Setting default values for PlayerPrefs");
+		//Screen Settings
+		Debug.Log ("Setting default width to value " + width);
+		PlayerPrefs.SetFloat ("defaultWidth", width);
+		Debug.Log ("Setting default height to value " + height);
+		PlayerPrefs.SetFloat ("defaultHeight", height);
+
+
+		//Multipayer Settings
+		Debug.Log ("Setting default ServerName to " + ServerName);
+		PlayerPrefs.SetString ("defaultServerName ", ServerName);
+		Debug.Log ("Setting default connectionPort to value " + connectionPort);
+		PlayerPrefs.SetString ("defaultPort", connectionPort);
+		Debug.Log ("Setting default playerHardcap to " + playersHardcap);
+		PlayerPrefs.SetString ("playerHardcap", playersHardcap);
+		Debug.Log ("Setting default maxPlayers to value " + maxPlayers);
+		PlayerPrefs.SetString ("defaultMaxPlayers", maxPlayers);
+		Debug.Log ("Setting RegisteredGameName to value " + registeredGameName);
+		PlayerPrefs.SetString ("registeredGameName", registeredGameName);
+
+		Debug.Log ("Defaults set");
+	}
+	
+}
