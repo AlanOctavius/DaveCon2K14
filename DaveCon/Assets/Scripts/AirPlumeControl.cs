26137d3ff9c0900d370a860444df59cebf4eeeb5
LevityDave <Seven_Years_Later@hotmail.com>
2014-06-09 07:55:48 +0100

    Added Fan and Air Plum
    It works

---
+++ DaveCon/Assets/Scripts/AirPlumeControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirPlumeControl : MonoBehaviour {
+
+	private GameObject Here;
+	public float Springforce = 20;
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		
+		other.GetComponent<PlayerController> ().Spring(Springforce);
+
+	}
+	
+	public void Destroy()
+	{
+		Network.Destroy (networkView.viewID);
+	}
+	
+}
